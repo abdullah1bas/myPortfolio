ad04f7fe9c5bba2f657532761827d28325a8bb2a
Abdullah Abazza <7a7abas@gmail.com>
2023-09-04 06:51:57 +0300

    Change img.png

--- src/components/3-main/myProjects.js
+++ src/components/3-main/myProjects.js
@@ -2,7 +2,7 @@ export const myProjects = [
   {
     projectTitle: "E-commerce",
     category: "react",
-    imgPath: "public/images/e-commerce.PNG",
+    imgPath: "public/images/e-commerce.png",
     description: "The e-commerce website is designed to showcase a wide range of commercial products. It offers a user-friendly interface for customers to browse and purchase items online. The website provides detailed product descriptions, high-quality images, and secure payment options to ensure a seamless shopping experience. With its intuitive navigation and responsive design, customers can easily find and buy products of their choice. The website also offers customer support and order tracking features to enhance customer satisfaction and loyalty.",
     linkPath: "https://ecommerce-823a2.firebaseapp.com",
     githubPath: "https://github.com/abdullah1bas/React-E-commerce-with-Strapi",
@@ -10,7 +10,7 @@ export const myProjects = [
   {
     projectTitle: "Admin Dashboard",
     category: "react",
-    imgPath: "public/images/admin-dashboard.PNG",
+    imgPath: "public/images/admin-dashboard.png",
     description: "The admin dashboard website is a comprehensive tool for managing online projects and tracking various metrics. It offers a range of customizable dashboard templates that provide real-time sales data, demographics, and other statistical information. These templates help businesses improve metrics reporting and make data-driven decisions. Users can choose from a variety of free and premium templates, each offering unique features and customization options. The templates are designed to be user-friendly and visually appealing, making it easy for admins to monitor and analyze data efficiently.",
     linkPath: "https://admin-dashboard-57acc.firebaseapp.com/",
     githubPath: "https://github.com/abdullah1bas/React-Admin-Dashboard",
@@ -18,7 +18,7 @@ export const myProjects = [
   {
     projectTitle: "Special Design",
     category: "JSBootstrap",
-    imgPath: "public/images/spicial-design.PNG",
+    imgPath: "public/images/spicial-design.png",
     description: "A complete project that brings together all my projects, and in this project there are all my capabilities and skills that I have learned.",
     linkPath: "https://abdullah1bas.github.io/Special-Design/",
     githubPath: "https://github.com/abdullah1bas/Special-Design",
@@ -26,7 +26,7 @@ export const myProjects = [
   {
     projectTitle: "Abazza",
     category: "JSBootstrap",
-    imgPath: "public/images/abazza.PNG",
+    imgPath: "public/images/abazza.png",
     description: "A fully responsive project with animations and a wide variety of sections",
     linkPath: "https://abdullah1bas.github.io/abazza/",
     githubPath: "https://github.com/abdullah1bas/abazza",
@@ -34,7 +34,7 @@ export const myProjects = [
   {
     projectTitle: "Login",
     category: "JSBootstrap",
-    imgPath: "public/images/login.PNG",
+    imgPath: "public/images/login.png",
     description: "Login page with a unique design",
     linkPath: "https://abdullah1bas.github.io/login/",
     githubPath: "https://github.com/abdullah1bas/login",
@@ -42,7 +42,7 @@ export const myProjects = [
   {
     projectTitle: "New Image Editor",
     category: "JSBootstrap",
-    imgPath: "public/images/new-image-editor.PNG",
+    imgPath: "public/images/new-image-editor.png",
     description: "Modify any image with any frame and then upload it",
     linkPath: "http://abdullah1bas.github.io/new-Image-Editor/",
     githubPath: "https://github.com/abdullah1bas/new-Image-Editor",
@@ -50,7 +50,7 @@ export const myProjects = [
   {
     projectTitle: "Old Image Editor",
     category: "JSBootstrap",
-    imgPath: "public/images/old-image-editor.PNG",
+    imgPath: "public/images/old-image-editor.png",
     description: "Modify any image with any frame and then upload it",
     linkPath: "https://abdullah1bas.github.io/old-image-editor/",
     githubPath: "https://github.com/abdullah1bas/old-image-editor",
@@ -58,7 +58,7 @@ export const myProjects = [
   {
     projectTitle: "hangman-Game",
     category: "js",
-    imgPath: "public/images/hangman.PNG",
+    imgPath: "public/images/hangman.png",
     description: "Hangman is an entertaining game that requires puzzle-solving and guessing skills. In this game, a random word is selected, and players must guess the letters correctly to reveal the complete word before running out of allotted attempts. Hangman is a fun challenge to improve language skills while also providing relaxation.",
     linkPath: "https://abdullah1bas.github.io/hangman-Game/",
     githubPath: "https://github.com/abdullah1bas/hangman-Game",
@@ -66,7 +66,7 @@ export const myProjects = [
   {
     projectTitle: "memory-Game",
     category: "js",
-    imgPath: "public/images/memory-game.PNG",
+    imgPath: "public/images/memory-game.png",
     description: "The memory game is a fun exercise for memory and focus. In this game, cards with different pictures or symbols are placed on the table, and players must find matching cards by flipping them over at the same time. This game requires the ability to concentrate, remember, and stimulate the brain.",
     linkPath: "https://abdullah1bas.github.io/memory-Game/",
     githubPath: "https://github.com/abdullah1bas/memory-Game",
@@ -74,7 +74,7 @@ export const myProjects = [
   {
     projectTitle: "typing-speed-Game",
     category: "js",
-    imgPath: "public/images/typing-speed-testing-game.PNG",
+    imgPath: "public/images/typing-speed-testing-game.png",
     description: "The typing speed game is a challenge to test a player's ability to type quickly and accurately. Players must complete the displayed text as fast as possible, comparing it with ideal speed and accuracy. This game is useful for improving keyboard typing skills and increasing speed and accuracy in typing.",
     linkPath: "https://abdullah1bas.github.io/typing-speed-Game/",
     githubPath: "https://github.com/abdullah1bas/typing-speed-Game",
@@ -82,7 +82,7 @@ export const myProjects = [
   {
     projectTitle: "Bondi",
     category: "CSSBootstrap",
-    imgPath: "public/images/Bondi.PNG",
+    imgPath: "public/images/Bondi.png",
     description: "A complete project showcasing my skills in HTML and CSS technologies.",
     linkPath: "https://abdullah1bas.github.io/Bondi/",
     githubPath: "https://github.com/abdullah1bas/Bondi",
@@ -90,7 +90,7 @@ export const myProjects = [
   {
     projectTitle: "Responsive Design",
     category: "CSSBootstrap",
-    imgPath: "public/images/responsive-design.PNG",
+    imgPath: "public/images/responsive-design.png",
     description: "A complete project showcasing my skills in HTML and CSS technologies and framework.",
     linkPath: "https://abdullah1bas.github.io/Responsive-Design/",
     githubPath: "https://github.com/abdullah1bas/Responsive-Design",
@@ -98,7 +98,7 @@ export const myProjects = [
   {
     projectTitle: "Dashboard",
     category: "CSSBootstrap",
-    imgPath: "public/images/dashboard.PNG",
+    imgPath: "public/images/dashboard.png",
     description: "A dashboard is a page that displays important indicators and statistics for the user's account or system in a comprehensive and concise manner.",
     linkPath: "https://abdullah1bas.github.io/Dash-Board/",
     githubPath: "https://github.com/abdullah1bas/Dash-Board",
@@ -106,7 +106,7 @@ export const myProjects = [
   {
     projectTitle: "Kasper",
     category: "css",
-    imgPath: "public/images/kasper.PNG",
+    imgPath: "public/images/kasper.png",
     description: "A complete project showcasing my skills in HTML and CSS technologies.",
     linkPath: "https://abdullah1bas.github.io/kasper/",
     githubPath: "https://github.com/abdullah1bas/kasper",
@@ -114,7 +114,7 @@ export const myProjects = [
   {
     projectTitle: "Leon",
     category: "css",
-    imgPath: "public/images/leon.PNG",
+    imgPath: "public/images/leon.png",
     description: "A complete project showcasing my skills in HTML and CSS technologies.",
     linkPath: "https://abdullah1bas.github.io/leon/",
     githubPath: "https://github.com/abdullah1bas/leon",
